cb952b3085e7cc79e6bfc31e2a88492eee80399e
Abdoul Tare <abdoultare@Abdouls-MacBook-Pro.local>
2020-02-22 20:45:46 -0500

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -41,19 +41,20 @@ class Directory extends React.Component {
                   id: 5,
                   linkUrl: 'shop/mens'
                 }
-              ] 
+            ] 
         }
     }
     render() {
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                this.state.sections.map(({id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))
             }
             </div>
         )
     }
 }
+
 export default Directory;
