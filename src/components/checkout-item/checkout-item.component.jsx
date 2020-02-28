a919a6d35402247a55ecab154060bc8e0637591b
Abdoul Tare <abdoultare@Abdouls-MacBook-Pro.local>
2020-02-28 16:38:06 -0500

    moved our directory and shop data into it own reducer, created respective selectors and upadated corresponding components with new redux flow fro directory/shop

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -14,11 +14,12 @@ const {name, imageUrl, price, quantity} = cartItem;
     </div>
     <span className='name'>{name}</span>
     <span className='quantity'>
-    <div className='arrow' onClick={() => removeItem(cartItem)}>&#9196;</div>
+    <div className='arrow' onClick={() => removeItem(cartItem)}>&#8681;</div>
     <span className='value'>{quantity}</span>
-    <div className='arrow' onClick={() => addItem(cartItem)}>&#9195;</div>
+    <div className='arrow' onClick={() => addItem(cartItem)}>&#8679;</div>
     </span>
-    <span className='price'>{price}</span>
+    <span className='price'>${price}</span>
+    <span className='sub-total'>${price*quantity}</span>
     <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10060;</div>
     </div>
 );
